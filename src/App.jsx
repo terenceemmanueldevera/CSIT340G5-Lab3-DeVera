d1047179ddd6fbcfe73913291d7cf330c77352eb
terenceemmanuel <terence.emmanuel@gmail.com>
2026-09-21 20:31:39 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
     </div>
   )
 }
@@ -31,12 +31,22 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'CSIT340 - Industry Elective'
-  const part1 = 'CSIT340 Web Development'
-  const exercises1 = 3
-  const part2 = 'CSIT321 App Development'
-  const exercises2 = 3
-  const part3 = 'CSIT327 Information Management'
-  const exercises3 = 3
+
+  const part1 = {
+    name : 'CSIT340 Web Development',
+    exercises : 3
+  }
+  
+  const part2 = {
+    name : 'CSIT321 App Development',
+    exercises : 3
+  }
+
+  const part3 = {
+    name : 'CSIT327 Information Management',
+    exercises : 3
+  }
+
 
   const studentName = 'Terence Emmanuel De Vera'
   const courseCode = 'CSIT340'
@@ -46,18 +56,13 @@ const App = () => {
     <div>
       <p></p>
 
-      <p> Exercise 1.2 : course information step 2</p>
+      <p> Exercise 1.3 : course information step 3</p>
      
       <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
       <Footer name={studentName} code={courseCode} section={section} />
     </div>
   )
 }
-
 export default App
